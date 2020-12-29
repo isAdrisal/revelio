ec0a27ea4af068394edab3e759c9a2f48a78635f
isAdrisal <29012873+isAdrisal@users.noreply.github.com>
2020-12-29 15:36:31 +1100

    Updates from testing

--- src/revelio.mjs
+++ src/revelio.mjs
@@ -1,4 +1,4 @@
-const revelio = (() => {
+const revelio = () => {
   // Return null if not executed in a browser.
   if (typeof document === 'undefined' || typeof window === 'undefined') {
     return null;
@@ -7,9 +7,8 @@ const revelio = (() => {
   // User variables.
   let variables = {};
   variables.root = window.revelioConfig && window.revelioConfig.root || null;
-  variables.rootMargin = root && window.revelioConfig && window.revelioConfig.rootMargin || null;
+  variables.rootMargin = variables.root && window.revelioConfig && window.revelioConfig.rootMargin || null;
   variables.threshold = window.revelioConfig && window.revelioConfig.threshold || 0.25;
-  variables.cssClasses = window.revelioConfig && window.revelioConfig.cssClasses || ['fade-in', 'slide-up'];
 
   // Find all DOM elements with [data-revelio] attribute. Return if none.
   const animatedElements = document.querySelectorAll('[data-revelio]');
@@ -18,38 +17,52 @@ const revelio = (() => {
   // Don't hide content from Google; no IntersectionObserver fallback.
   const googlebot = navigator.userAgent.toLowerCase().includes('googlebot');
   if (window.IntersectionObserver === null || googlebot) {
-    animatedElements.forEach(element => {
-      variables.cssClasses.forEach(cssClass =>
-        element.classList.remove(cssClass));
-    });
+    animatedElements.forEach(element => element.dataset.revelio = '');
     return;
   };
   
   // IntersectionObserver callback function.
   const handleIntersection = (entries, observer) => {
+    let vw, vh = null;
+    if (variables.root === null) {
+      const html = document.querySelector('html');
+      vw = html.clientWidth;
+      vh = html.clientHeight;
+    };
     entries.forEach(entry => {
+      // Fix for intermittent Chrome bug with improperly calculated
+      // viewport size on first callback instance.
+      if (vw && vh) {
+        const bounds = entry.rootBounds;
+        if (bounds.width !== vw || bounds.height !== vh) return;
+      };
+
+      // Skip non-intersecting elements on first callback instance.
       if (entry.isIntersecting === false) return;
+      // Apply animations.
       const target = entry.target;
       const styles = getComputedStyle(target);
       const display = styles.getPropertyValue('display');
       const visibility = styles.getPropertyValue('visibility');
       requestAnimationFrame(() => {
-        // Skip processing non-visible elements.
+        // Skip processing non-visible elements. isVisible property only
+        // available in Chromium-based browsers with ioV2, so unused.
         if (display == 'none' || visibility == 'hidden') return;
-        variables.cssClasses.forEach(cssClass =>
-          element.classList.remove(cssClass));
+        target.dataset.revelio = '';
         observer.unobserve(target);
       });
     });
   };
   
-  const options = {
+  let options = {
     root: variables.root,
-    rootMargin: variables.rootMargin,
     threshold: variables.threshold
+  };
+  if (variables.rootMargin) {
+    options.rootMargin = variables.rootMargin
   };
   const observer = new IntersectionObserver(handleIntersection, options);
   animatedElements.forEach(element => observer.observe(element));
-})();
+};
 
 export { revelio };
